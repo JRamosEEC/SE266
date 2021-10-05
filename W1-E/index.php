17126a050d7bb298a55e81f01fd3424e056d6173
Ramos <008011005@N358-C3.local>
2021-10-05 14:45:51 -0400

    E completed

---
+++ W1-E/index.php
@@ -0,0 +1,12 @@
+<?php //Begin php statement
+
+//Declare an array of tasks and assign the key values
+$task = [
+    'title' => 'Go for a run', 
+    'due' => '8pm Today', 
+    'assigned_to' => 'Lily', 
+    'completed' => false
+];
+
+//Send code to index view php code
+require 'index.view.php';
