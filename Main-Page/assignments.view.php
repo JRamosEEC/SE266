9d68f1dd916cc5113de4be36b87ea2748f810a3e
Ramos <008011005@N358-C3.local>
2021-10-25 13:30:08 -0400

    main page updated

--- Main-Page/assignments.view.php
+++ Main-Page/assignments.view.php
@@ -38,8 +38,8 @@
         <div class="title col-sm-12 center-vertical">Assignments</div>
     
         <ul>
-            <li><a href="../W1-FizzBuzz/index.php" role="button" target="_blank">Week 1 - Fizz Buzz</a></li>
-            <li><a href="#" role="button" target="_blank">Week 2 - Patient</a></li>
+            <li><a href="../W1-FizzBuzz" role="button" target="_blank">Week 1 - Fizz Buzz</a></li>
+            <li><a href="../W2-PatientForm" role="button" target="_blank">Week 2 - Patient</a></li>
             <li><a href="#" role="button" target="_blank">Week 3 - ATM</a></li>
             <li><a href="#" role="button" target="_blank">Week 4</a></li>
             <li><a href="#" role="button" target="_blank">Week 5</a></li>
