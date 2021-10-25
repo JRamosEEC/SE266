f8de2b1314cf377d49cba4ec395c641bbabc2ae4
Ramos <008011005@N358-C3.local>
2021-10-25 14:35:28 -0400

    w3 atm started

---
+++ W3-ATM/index.view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+    <style>
+        header{
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+
+        .wrapper {
+            display: grid;
+            grid-template-columns: 180px 300px;
+        }
+
+        label{
+            text-align: right;
+            padding-right: 10px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<header>ATM</header>
+
+<body>
+    <form name="ATM" method="POST">
+        <!---<?php if(!empty($_POST)){ echo $validateString; } ?> --->
+            
+    </form>
+</body>
+
+</html>
